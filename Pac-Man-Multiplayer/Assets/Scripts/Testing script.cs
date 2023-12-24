d863c91a3b66be81e429ca09d27f617d16d54c62
Blerton Ismaili <ismaili.blerton.03@gmail.com>
2023-12-24 23:16:57 +0100

    Simple movement

--- Pac-Man-Multiplayer/Assets/Scripts/Testing script.cs
+++ Pac-Man-Multiplayer/Assets/Scripts/Testing script.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class Testingscript : MonoBehaviour
 {
+    public float speed = 5f;
+    private Rigidbody2D rb;
+    private Vector2 player_input;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        player_input.x = Input.GetAxisRaw("Horizontal");
+        player_input.y = Input.GetAxisRaw("Vertical");
+
+        player_input.Normalize();
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = speed * player_input * Time.deltaTime;
     }
 }
