a9c452f897b5c28dc7cd64108e3c30aafda1fa83
Blerton Ismaili <ismaili.blerton.03@gmail.com>
2023-12-30 20:53:13 +0100

    Created classic scene
    Imported some assets, improved the player controller, made the classic pacman map

--- Pac-Man-Multiplayer/Assets/Scripts/MovementController.cs
+++ Pac-Man-Multiplayer/Assets/Scripts/MovementController.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 public class MovementController : MonoBehaviour
 {
     private Vector2 direction = Vector2.zero;
+    private Vector2 player_input = Vector2.zero;
     private Rigidbody2D rb;
-
     public float speed = 5f;
 
     void Start()
@@ -16,6 +16,7 @@ public class MovementController : MonoBehaviour
     void Update()
     {
         UserInput();
+        MovePlayer();
     }
 
     private void FixedUpdate()
@@ -23,19 +24,36 @@ public class MovementController : MonoBehaviour
         rb.velocity = speed * direction * Time.deltaTime;
     }
 
+    // Checks if the way that the player wants to go is free, and when it is it starts walking there
+    private void MovePlayer()
+    {
+        if (player_input == Vector2.left &&
+            !Physics2D.BoxCast(transform.position, GetComponent<BoxCollider2D>().size, 0, Vector2.left, 0.1f))
+            direction = player_input;
+        if (player_input == Vector2.right &&
+            !Physics2D.BoxCast(transform.position, GetComponent<BoxCollider2D>().size, 0, Vector2.right, 0.1f))
+            direction = player_input;
+        if (player_input == Vector2.up &&
+            !Physics2D.BoxCast(transform.position, GetComponent<BoxCollider2D>().size, 0, Vector2.up, 0.1f))
+            direction = player_input;
+        if (player_input == Vector2.down &&
+            !Physics2D.BoxCast(transform.position, GetComponent<BoxCollider2D>().size, 0, Vector2.down, 0.1f))
+            direction = player_input;
+    }
+
     // Gets the user input and changes the direction of the player
     private void UserInput()
     {
         if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
-            direction = Vector2.left;
+            player_input = Vector2.left;
 
         if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
-            direction = Vector2.right;
+            player_input = Vector2.right;
 
         if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
-            direction = Vector2.up;
+            player_input = Vector2.up;
 
         if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
-            direction = Vector2.down;
+            player_input = Vector2.down;
     }
 }
